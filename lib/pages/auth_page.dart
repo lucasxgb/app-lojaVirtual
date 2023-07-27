bca8fe6cd6c2b049b28b12408faa90ecfa47d238
Lucas Gabriel <68016822+lucasxgb@users.noreply.github.com>
2023-07-27 14:24:33 -0300

    Aplicação e Animações Finalizadas

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:shop/components/auth_form.dart';
 
@@ -18,40 +17,45 @@ class AuthPage extends StatelessWidget {
               ], begin: Alignment.topLeft, end: Alignment.bottomRight),
             ),
           ),
-          Container(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 50,
-                  ),
-                  //cascade operator
-                  //transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.deepOrange.shade900,
-                    boxShadow: const [
-                      BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, 2))
-                    ],
-                  ),
-                  child: Text(
-                    'Minha loja',
-                    style: TextStyle(
-                        fontFamily: 'Anton',
-                        fontSize: 45,
-                        color: Theme.of(context).textTheme.titleLarge?.color),
-                  ),
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 50,
+                      ),
+                      //cascade operator
+                      //transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.deepOrange.shade900,
+                        boxShadow: const [
+                          BoxShadow(
+                              blurRadius: 8,
+                              color: Colors.black26,
+                              offset: Offset(0, 2))
+                        ],
+                      ),
+                      child: Text(
+                        'Minha loja',
+                        style: TextStyle(
+                            fontFamily: 'Anton',
+                            fontSize: 45,
+                            color:
+                                Theme.of(context).textTheme.titleLarge?.color),
+                      ),
+                    ),
+                    AuthForm(),
+                  ],
                 ),
-                AuthForm(),
-              ],
+              ),
             ),
           )
         ],
